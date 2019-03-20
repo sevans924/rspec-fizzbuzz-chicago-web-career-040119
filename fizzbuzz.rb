241b434b6f3db53f1a7362c981273459e38131e5
Sarah Elzinga <sevans924@gmail.com>
2019-03-20 20:16:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(number)
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
-  elseif number % 3 == 0 && number % 5 == 0
+  elsif number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
   else
 end
