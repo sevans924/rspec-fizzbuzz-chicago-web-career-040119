4bc02df714fd7c72d672b742aba8114302981d79
Sarah Elzinga <sevans924@gmail.com>
2019-03-20 20:13:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@ def fizzbuzz(number)
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0
+  elsif number % 3 == 0 && % 5 == 0
     return "FizzBuzz"
   else
-end  
+end
 end# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
