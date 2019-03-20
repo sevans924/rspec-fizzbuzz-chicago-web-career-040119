7d77efb96fbc3895fc484b0430a3cdef308c5e58
Sarah Elzinga <sevans924@gmail.com>
2019-03-20 20:16:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,6 @@ def fizzbuzz(number)
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
-
 end
 end# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
