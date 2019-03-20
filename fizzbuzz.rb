b34f1ea635246b040dc284778e675343d64dc52c
Sarah Elzinga <sevans924@gmail.com>
2019-03-20 20:16:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number % 3 == 0 && number % 5 == 0
+    return "FizzBuzz"
+    elsif number % 3 == 0
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0
-    return "FizzBuzz"
-  else
+
 end
 end# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
